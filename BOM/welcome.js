4aa63426f93c9d8ea8028e6a739ba7eae12c8477
chandra <chandragulo77@gmail.com>
2025-09-20 11:22:17 +0700

    first Commit

---
+++ BOM/welcome.js
@@ -0,0 +1,33 @@
+/* const tamu = prompt("Siapakah anda?");
+alert("Selamat Datang, " + tamu + "!"); */
+
+// alert("Halo, user!");
+
+// let message = "Hallo user";
+// alert(message);
+
+// BOM: Method Prompt
+
+// let pesanInput = prompt("Silakan masukkan pesan yang kamu suka!");
+// console.log(pesanInput);
+
+// let pesanInput = prompt("Silakan masukkan angka...");
+// console.log(`Tipe data dari \`pesanInput\` adalah ${typeof pesanInput}`);
+
+// let pesanInput = prompt("Silakan masukkan angka yang Anda suka!");
+// let pesanInputConvertedToNumber = Number(pesanInput);
+// console.log(
+//   `Tipe data dari \`pesanInputConvertedToNumber\` adalah ${typeof pesanInputConvertedToNumber}`
+// );
+
+// let name = prompt("Silahkan masukan nama anda", "chandra ");
+// console.log(name);
+
+// cara memanggil BOM
+function alert(nama) {
+  console.log("hati hati" + nama);
+}
+
+alert("chewbacca");
+
+window.alert("chewnacca");
